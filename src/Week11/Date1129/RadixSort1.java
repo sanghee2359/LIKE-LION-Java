179c9776d2340b6dbc4984f2f35b2fc419f77d0f
sanghee <wjdtkdgml7352@daum.net>
2022-11-29 10:48:22 +0900

    [기수정렬] 2.배열 안에서 가장 높은 자릿수를 찾고 자릿수의 중복을 제거

--- src/Week11/Date1129/RadixSort1.java
+++ src/Week11/Date1129/RadixSort1.java
@@ -1,41 +1,48 @@
 package Week11.Date1129;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
+import java.util.*;
 
-// 배열 10개를 만들고
-// 7,4,5,9,1,0 를 배열의 인덱스에 맞게 넣고
-// 앞에서부터 꺼내서 정렬하는 코드를 만들어 보세요.
-
-
-//가장 큰 숫자는 1000000000(0이 9개)입니다. 그러면 가장 큰 숫자만큼 배열을 생성하면 문제가 없을까요?
-// : 정렬해야 할 숫자가 10개 밖에 없다면 혹은 1만개 2만개라면 어떨까요? 공간이 많이 낭비  됩니다.
-// 1. 일의 자리수로 정렬
-// 2. 십의 자리수로 정렬
+// 2. 출력받은 arr을 다시 10의 자리에서 정렬 (arr = [0, 1, 2, 13, 14, 16, 7, 7, 8, 18, 223])
 // 3. 가장 높은 자릿수를 찾고 그만큼 자릿수 정렬을 반복해줘야한다.
+
+//arr 각 수의 자릿수 [1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 3] —> 중복제거 [1, 2, 3]
 public class RadixSort1 {
+    public static int[] getDigits(int []arr){
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            // 중복 제거하고 자릿수 구하기
+            if(arr[i] == 0) set.add(1); // 자릿수 1 과 2
+            else set.add((int)(Math.log10(arr[i])+1));
+        }
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for(Integer num : set){
+            answer[idx++] = num;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[]{2, 8, 18, 13, 1, 7, 16, 7, 0, 14};
-//        int[] redix = new int [10];
+        int[] arr = new int[]{2, 8, 18, 13, 1, 7, 16, 7, 0, 14, 223};
+
         Queue<Integer>[] queueArr = new Queue[10];
         for (int i = 0; i < queueArr.length; i++) {
             queueArr[i] = new ArrayDeque<>(); // 덱
         }
-        /* 이 방법도 사용 가능
-        for(var i : arr) {
-            queueArr[i % 10].add(i); // i % 10해서 일의 자리를 구함
-        }*/
+        // 1의 자리로 분류
         for (int i = 0; i < arr.length; i++) {
-            queueArr[arr[i]%10].add(arr[i]);
+            queueArr[arr[i] % 10].add(arr[i]);
         }
         // 꺼내서 arr에 다시 넣는다.
         int idx = 0;
         for (int i = 0; i < queueArr.length; i++) {
-            while(!queueArr[i].isEmpty()){
+            while (!queueArr[i].isEmpty()) {
                 arr[idx++] = queueArr[i].poll(); // 0번째부터 꺼내 arr에 넣는다.
             }
         }
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(getDigits(arr)));
+
     }
 }
