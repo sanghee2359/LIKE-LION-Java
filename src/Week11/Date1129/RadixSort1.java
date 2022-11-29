f667c9c645beaf2d73f7c3d466b4be2da26a0884
sanghee <wjdtkdgml7352@daum.net>
2022-11-29 10:14:58 +0900

    [기수정렬] 1.일의 자리수로 정렬

--- src/Week11/Date1129/RadixSort1.java
+++ src/Week11/Date1129/RadixSort1.java
@@ -1,6 +1,8 @@
 package Week11.Date1129;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 
 // 배열 10개를 만들고
 // 7,4,5,9,1,0 를 배열의 인덱스에 맞게 넣고
@@ -14,18 +16,26 @@ import java.util.Arrays;
 // 3. 가장 높은 자릿수를 찾고 그만큼 자릿수 정렬을 반복해줘야한다.
 public class RadixSort1 {
     public static void main(String[] args) {
-        int[] arr = new int []{7,4,5,9,1,0};
-        int[] redix = new int [10];
-        Arrays.fill(redix, -1);
+        int[] arr = new int[]{2, 8, 18, 13, 1, 7, 16, 7, 0, 14};
+//        int[] redix = new int [10];
+        Queue<Integer>[] queueArr = new Queue[10];
+        for (int i = 0; i < queueArr.length; i++) {
+            queueArr[i] = new ArrayDeque<>(); // 덱
+        }
         /* 이 방법도 사용 가능
         for(var i : arr) {
-            redix[i] = i;
+            queueArr[i % 10].add(i); // i % 10해서 일의 자리를 구함
         }*/
         for (int i = 0; i < arr.length; i++) {
-            redix[arr[i]] = arr[i];
+            queueArr[arr[i]%10].add(arr[i]);
         }
-        for(int x : redix) {
-            if(x != -1) System.out.println(x);
+        // 꺼내서 arr에 다시 넣는다.
+        int idx = 0;
+        for (int i = 0; i < queueArr.length; i++) {
+            while(!queueArr[i].isEmpty()){
+                arr[idx++] = queueArr[i].poll(); // 0번째부터 꺼내 arr에 넣는다.
+            }
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
