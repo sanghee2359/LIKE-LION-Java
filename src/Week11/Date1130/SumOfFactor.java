83658b01b035e1a6ab48b7f8432ffb70a3eaa23c
sanghee <wjdtkdgml7352@daum.net>
2022-11-30 10:14:30 +0900

    [리스트] 약수의 합 구하기

---
+++ src/Week11/Date1130/SumOfFactor.java
@@ -0,0 +1,25 @@
+package Week11.Date1130;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//입출력 예 #1
+  //      12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.
+public class SumOfFactor {
+    public static int sum(int n) {
+        List<Integer> factor = new ArrayList<>();
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) factor.add(i);
+        }
+        for(int x : factor) {
+            answer += x;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(sum(n));
+    }
+}
