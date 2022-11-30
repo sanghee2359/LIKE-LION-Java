e3f64e60e565d86494e31ab34e16c2f6d8f84e54
sanghee <wjdtkdgml7352@daum.net>
2022-11-30 10:54:27 +0900

    [문제 분석] 두 노드간의 거리

--- src/Week11/Date1130/FindParent.java
+++ src/Week11/Date1130/FindParent.java
@@ -3,12 +3,13 @@ package Week11.Date1130;
 public class FindParent {
     public static int count;
     public static int goUp(int a, int b) {
-        if(a>b) {
-            return goUp(a / 2, b)+1; // a를 부모 쪽으로
-//            count++;
-        } else if(b>a) {
-            return goUp(a,b/2)+1; // b를 부모 쪽으로
-//            count++;
+        // 올라간 만큼 1씩 올라가기 때문에 a == b가 되었을 때 출력되는 값이 두 노드간의 거리이다.
+        // 부모가 같을때까지 부모를 찾으면서 한칸 올라갈때마다 +1
+        // a = 3일 경우, 3의 부모는 1이다.
+        if(a > b) {
+            return goUp(a / 2, b) + 1; // a를 부모 쪽으로
+        } else if(b > a) {
+            return goUp(a,b/2) + 1; // b를 부모 쪽으로
         } else return 0;
     }
     public static void main(String[] args) {
