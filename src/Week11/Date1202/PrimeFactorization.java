a3c53bb5da07583ec0bdd0bc378fbfea84da8049
sanghee <wjdtkdgml7352@daum.net>
2022-12-02 09:35:00 +0900

    [CodeUp] 소인수 분해 - 제출완료

--- src/Week11/Date1202/PrimeFactorization.java
+++ src/Week11/Date1202/PrimeFactorization.java
@@ -13,13 +13,14 @@ public class PrimeFactorization {
         int N = sc.nextInt();
 
         //1. 2부터 for문으로 나눈다.
-        for (int i = 2; i*i <= N; i++) {
+        for (int i = 2; i*i <= N; i++) { // 2. a  * b일 때 최소 하나는 N 미만이다. 그 최소값중 가장 큰 i를 설정
+            // 3. 해당 i를 N으롤 나머지 연산 했을 때 0이 나오면, N % i != 0 일때까지 반복한다.
             while (N % i == 0) {
                 System.out.println(i);
                 N = N / i;
             }
         }
-        if (N != 1)
+        if (N != 1) // 4. 나머지가 1이 아니면 앞에서 나누고 남은 N을 출력한다.
             System.out.printf("%d\n", N);
     }
 }
