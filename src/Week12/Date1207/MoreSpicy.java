89080ca3291c44143d6de539b96fdb6fb8c49044
sanghee <wjdtkdgml7352@daum.net>
2022-12-09 01:58:36 +0900

    [Heap] 더 맵게 - 프레임 작성

---
+++ src/Week12/Date1207/MoreSpicy.java
@@ -0,0 +1,15 @@
+package Week12.Date1207;
+//스코빌 지수를 K 이상으로 만들기 위해 Leo는
+// 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
+// Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+
+public class MoreSpicy {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+
+
+    }
+}
