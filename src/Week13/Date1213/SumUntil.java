18b525e96e007a00d10313337e91a83cb1734c8a
sanghee <wjdtkdgml7352@daum.net>
2022-12-14 09:37:40 +0900

    [최소비용경로] i=3의 From to 인덱스까지 값 더하기

--- src/Week13/Date1213/SumUntil.java
+++ src/Week13/Date1213/SumUntil.java
@@ -7,13 +7,14 @@ public class SumUntil {
     public static void main(String[] args) {
         int[] list = {2,7,40,19};
         int[][] dp = new int[list.length][list.length];
-        int i = 0;
-//        for (int i = 0; i < list.length; i++) {
-            for (int j = 0; j < list.length; j++) {
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = i; j < list.length; j++) {
                 if(j == 0) dp[i][j] = list[j];
                 else dp[i][j] = dp[i][j-1]+list[j];
             }
+            Arrays.stream(list).min();
             System.out.println(Arrays.toString(dp[i]));
-//        }
+        }
     }
 }
